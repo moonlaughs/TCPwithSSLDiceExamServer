0fd465b84329e1ae8bab12ab64d6c32208018ee0
Izabela Kuzniar <iza8848@gmail.com>
2019-01-07 12:35:31 +0100

    Add project files.

---
+++ TCPwithSSLDiceExamServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPwithSSLDiceExamServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
